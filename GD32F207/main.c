80040ae6e05483c9cbefdae6ae0ec7f96ec12c70
strongercjd <strongercjd@163.com>
2022-02-03 21:27:39 +0800

    修改单词拼写错误
    Signed-off-by: strongercjd <strongercjd@163.com>

--- GD32F207/main.c
+++ GD32F207/main.c
@@ -90,7 +90,7 @@ int main(void)
   printf("\r\n======================================================================");
   printf("\r\n=               (C) COPYRIGHT 2022                                   =");
   printf("\r\n=                                                                    =");
-  printf("\r\n=                GD32F20x LVGVL                                      =");
+  printf("\r\n=                GD32F20x LVGL                                      =");
   printf("\r\n=                                                                    =");
   printf("\r\n=                                           By Firefly               =");
   printf("\r\n======================================================================");
